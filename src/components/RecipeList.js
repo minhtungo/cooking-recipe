666aaca06f6135ffa4830c0fd8a442f0ea2fac39
minhtungo <loducfc@gmail.com>
2022-02-20 18:32:25 -0500

    add change nav color, dark/light theme

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,11 +3,17 @@ import './RecipeList.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useTheme } from '../hooks/useTheme';
+
 const RecipeList = ({ recipes }) => {
+  const { mode } = useTheme();
+  if (recipes.length === 0) {
+    return <div className='error'>No recipes to load...</div>;
+  }
   return (
     <div className='recipe-list'>
       {recipes.map((recipe) => (
-        <div key={recipe.id} className='card'>
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to make.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,14 +1,16 @@
 // styles
 import { useParams } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
+import { useTheme } from '../../hooks/useTheme';
 import './Recipe.css';
 
 export default function Recipe() {
   const { id } = useParams();
   const url = 'http://localhost:3000/recipes/' + id;
   const { data: recipe, isPending, error } = useFetch(url);
+  const { mode } = useTheme();
   return (
-    <div className='recipe'>
+    <div className={`recipe ${mode}`}>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
       {recipe && (
@@ -20,7 +22,7 @@ export default function Recipe() {
               <li key={ing}>{ing}</li>
             ))}
           </ul>
-          <p className="method">{recipe.method}</p>
+          <p className='method'>{recipe.method}</p>
         </>
       )}
     </div>
